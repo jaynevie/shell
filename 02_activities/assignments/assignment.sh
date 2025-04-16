1e38381f19199dffb0f2b417691bdf14e31d5ee9
jaynevie <jayneviem@gmail.com>
2025-04-15 20:59:18 -0400

    edited errors again, completed assignment 1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,7 @@ ls data/raw
 cd data 
 mkdir processed
 cd processed 
-mkdir server_
+mkdir server_server_log user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ../..
 cp data/raw/*server*.log data/processed/server_logs
